48ce5a9659ebb3477d408d10b7d86946530a58b0
Vladislav Ivanov <103313657+Vladisav2233@users.noreply.github.com>
2022-07-18 00:26:10 +0300

    Add files via upload

---
+++ WinndowsForms/Lab1.2_1.3 NonRectangularInheritedForm/nForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab1._2_1._3_NonRectangularInheritedForm
+{
+    public partial class nForm : Lab1._2_1._3_NonRectangularInheritedForm.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
