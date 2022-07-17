3d493cb46c74b9843e3618091087b237e98cabb6
Vladislav Ivanov <103313657+Vladisav2233@users.noreply.github.com>
2022-07-18 01:38:10 +0300

    Add files via upload

---
+++ WPF/Lab4.3 UserIn DockPanel and GridSplitt/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab4._3_UserIn_DockPanel_and_GridSplitt
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (FontFamily F in Fonts.SystemFontFamilies)
+            {
+                ListBox1.Items.Add(F.ToString());
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox1.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Bold);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            richTextBox1.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
+        }
+
+        private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+        ValueChanged:
+            try
+            {
+                richTextBox1.Selection.ApplyPropertyValue(FontSizeProperty,
+                Slider1.Value.ToString());
+            }
+            catch { }
+        }
+
+        private void ListBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            richTextBox1.Selection.ApplyPropertyValue(FontFamilyProperty, ListBox1.SelectedItem);
+        }
+    }
+}
